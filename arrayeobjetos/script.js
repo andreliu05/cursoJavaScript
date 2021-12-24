13558e0ecae92ca0f6619bbebd1d11ee7e1b4cb2
andreliu05 <andreliu05@gmail.com>
2021-12-24 10:49:23 -0300

    andamento do curso

---
+++ arrayeobjetos/script.js
@@ -0,0 +1,9 @@
+//O que são vetors ou Arrays 
+// Como declarar um array 
+
+//let array = ['string', 1, true];
+//console.log(array);
+
+//pode guardar varios tipos de dados 
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array);
---
+++ variaveis/script.js
@@ -0,0 +1,166 @@
+//Boolean
+var vOuF= false;
+console.log(typeof(vOuF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer))
+
+//String
+var nome = 'diana';
+console.log(typeof(nome))
+
+// como declarar uma var
+
+var variavel = 'diana';
+variavel = 'martine';
+console.log(variavel);
+
+// como declarar uma let
+
+let variavel2 = 'diana';
+variavel2= 'martine';
+console.log(variavel2);
+
+//como declarar uma const
+const constante = 'diana';
+console.log(constante);
+
+//escopo global 
+
+var escopoGlobal = 'global';
+console.log(escopoGlobal)
+
+//escopo local 
+
+function escopolocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+escopolocal();
+
+//atribuição 
+
+var atribuicao = 'andre';
+
+// comparação
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+//comparacao identica 
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);
+
+//adição 
+var adicao = 3 + 4;
+console.log(adicao);
+
+//subtração 
+var subtrair = 7 - 2;
+console.log(subtrair);
+
+//multiplicação 
+var multiplicar = 8 * 4;
+console.log(multiplicar);
+
+//divisão Real
+var dividir = 29 / 3;
+
+//divisao inteira 
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira)
+
+// potenciacao 
+var potenciacao = 2 ** 10;
+console.log(potenciacao)
+
+// maior que 
+var maiorQue = 3 > 7;
+console.log(maiorQue)
+
+// menor que
+var menorQue = 16 < 16 
+console.log(menorQue)
+
+// maior ou igual a 
+var maiorOuIgual = 29 >= 28
+
+// menor ou igual a 
+var menorOuIgual = 28 <= 30
+
+//Operadores logicos 
+
+var e = true && true;
+console.log(e)
+
+var ou = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
