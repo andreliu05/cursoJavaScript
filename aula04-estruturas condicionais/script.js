cc4c28a767af9cb836f17451d7f3f3f6d7c68dfb
andreliu05 <andreliu05@gmail.com>
2021-12-24 16:47:04 -0300

    andamento do curso

---
+++ aula04-estruturas condicionais/script.js
@@ -0,0 +1,47 @@
+//var jogador1 = 0
+//var jogador2 = 0
+//var placar; 
+
+// Usando if Ternário
+//jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são válidos') : 
+//console.log('jogadores Inválidos')
+
+//Usando 'if'
+//if (jogador1 > 0 && jogador2 == 0) {
+    //console.log('jogador 1 marcou ponto!')
+    //placar = jogador1 > jogador2;
+
+
+//Usando 'else if'
+//else if (jogador2 > 0 && jogador1 == 0 ) {
+    //console.log('Jogador2 marcou ponto')
+    //placar = jogador2 > jogador1;
+
+
+//Usando 'else'
+//else {
+    //console.log('ninguém marcou pontos')
+
+
+//switch (placar) {
+    //case placar = jogador1 > jogador2:
+        //console.log('Jogador1 venceu')
+        //break;
+    //case placar = jogador2 > jogador1:
+        //console.log('Jogador2 venceu');
+        //break;
+    //default:
+            //console.log('ninguem ganhou')
+
+
+let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5']
+
+let object = { propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3'
+
+}
+
+// for executa uma instrução até que ela seja falsa
+
+for (let indice = 0; indice < array.length; indice++) {
+    console.log(indice)
+} 
