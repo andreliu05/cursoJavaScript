2976ceebcd61fc6aa4b889b22d6ed8f53c620570
andreliu05 <andreliu05@gmail.com>
2021-12-25 10:10:27 -0300

    curso js

--- aula04-estruturas condicionais/script.js
+++ aula04-estruturas condicionais/script.js
@@ -42,6 +42,38 @@ let object = { propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'va
 
 // for executa uma instrução até que ela seja falsa
 
-for (let indice = 0; indice < array.length; indice++) {
-    console.log(indice)
-} 
+//for (let indice = 0; indice < array.length; indice++) {
+    //console.log(indice)
+ 
+
+// for/in funciona como uma repeitção a partir de uma propriedade
+
+//for (let  i in array) {
+    //console.log(i)
+//}
+
+//com object
+
+//for (let i in object) {
+    //console.log(i)
+//}
+
+//for (i of array) {
+    //console.log(i)
+//}
+
+//com object
+//for (i of object.propriedade1) {
+    //console.log(i)
+//}
+
+var a = 0
+while (a < 10) {
+    a++
+    console.log(a)
+}
+
+do {
+    a++ 
+    console.log(a)
+} while ( a < 10)
